c1770f6cb3768b1dcb309920e49ad48f994c0fb8
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2020-12-09 22:52:30 -0500

    Update etnicaHome.js

--- controllers/etnicaHome.js
+++ controllers/etnicaHome.js
@@ -197,6 +197,7 @@ var controller = {
         console.log(params);
         let rename='';
         const splited = params.split('_');
+        console.log(splited);
         if(splited.length===1){
             console.log('noUnderscore');
             const name = params.split('.')[0];
